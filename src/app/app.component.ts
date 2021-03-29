cfe50b573c29b6b8bbdf99f5f5d0b44b85ce638c
Surekha Biradar <surekha.biradar@ibm.com>
2021-03-29 16:45:14 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'design-studio';
+  constructor(){   
+  }
+}
